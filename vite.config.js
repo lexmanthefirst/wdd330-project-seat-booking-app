a8e89ae465d90654a4ded78550d6b90db9b8773f
Okhitoya Alex <lexmanthefirst@gmail.com>
2025-04-13 20:42:07 -0400

    updated project build

--- vite.config.js
+++ vite.config.js
@@ -10,45 +10,18 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-      },
-      output: {
-        assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split(".");
-          const ext = info[info.length - 1];
-          if (/\.(png|jpe?g|gif|svg|webp|ico)$/.test(assetInfo.name)) {
-            return `assets/images/[name]-[hash][extname]`;
-          }
-          if (/\.(woff2?|eot|ttf|otf)$/.test(assetInfo.name)) {
-            return `assets/fonts/[name]-[hash][extname]`;
-          }
-          return `assets/[name]-[hash][extname]`;
-        },
+        header: resolve(__dirname, "src/public/partials/header.html"),
+        footer: resolve(__dirname, "src/public/partials/footer.html"),
       },
     },
-    copyPublicDir: true,
   },
   server: {
-    port: 3000,
+    port: 5713,
     open: true,
   },
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
-      "@components": resolve(__dirname, "./src/components"),
-      "@utils": resolve(__dirname, "./src/utils"),
-      "@api": resolve(__dirname, "./src/api"),
-      "@public": resolve(__dirname, "./src/public"),
     },
   },
-  optimizeDeps: {
-    include: ["path"],
-  },
-  assetsInclude: [
-    "**/*.svg",
-    "**/*.png",
-    "**/*.jpg",
-    "**/*.jpeg",
-    "**/*.gif",
-    "**/*.webp",
-  ],
 });
